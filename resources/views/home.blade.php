f6979ccf0c5b741bce0b893b5ea682f5627f66aa
juan torres <juanonfleekmedia@gmail.com>
2023-10-19 18:51:47 -0500

    contact

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -411,7 +411,7 @@
             <h1 class="mb-3">Contacto para cualquier consulta</h1>
             
         </div>
-        <div class="contact-detail position-relative p-5">
+        <div class="contact-detail position-relative p-5 contact">
             <div class="row g-5 mb-5 justify-content-center">
                 <div class="col-xl-4 col-lg-6 wow fadeIn" data-wow-delay=".3s">
                     <div class="d-flex bg-light p-3 rounded">
@@ -420,7 +420,7 @@
                         </div>
                         <div class="ms-3">
                             <h4 class="text-primary">Dirección</h4>
-                            <a href="https://goo.gl/maps/Zd4BCynmTb98ivUJ6" target="_blank" class="h5">Lima, Perú</a>
+                            <a href="https://goo.gl/maps/Zd4BCynmTb98ivUJ6" target="_blank" class="h5 contact-h6">Lima, Perú</a>
                         </div>
                     </div>
                 </div>
@@ -442,19 +442,19 @@
                         </div>
                         <div class="ms-3">
                             <h4 class="text-primary">Email</h4>
-                            <a class="h5" href="mailto:info@example.com" target="_blank">info@grupotyg.com</a>
+                            <a class="h5 contact-h6" href="mailto:info@example.com" target="_blank">informes@grupotyg.com</a>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="row g-5">
                 <div class="col-lg-6 wow fadeIn" data-wow-delay=".3s">
-                    <div class="p-5 h-100 rounded contact-map">
+                    <div class="p-5 h-100 rounded contact-map contact">
                         <iframe class="rounded w-100 h-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.4710403339755!2d-73.82241512404069!3d40.685622471397615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c26749046ee14f%3A0xea672968476d962c!2s123rd%20St%2C%20Queens%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1686493221834!5m2!1sen!2sbd" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
                 <div class="col-lg-6 wow fadeIn" data-wow-delay=".5s">
-                    <div class="p-5 rounded contact-form">
+                    <div class="p-5 rounded contact-form contact">
                         <div class="mb-4">
                             <input type="text" class="form-control border-0 py-3" placeholder="Su nombre">
                         </div>
