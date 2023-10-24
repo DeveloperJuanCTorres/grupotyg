da9ad3affc0847b5e536c74f5569e828f5945316
juan torres <juanonfleekmedia@gmail.com>
2023-10-24 12:50:35 -0500

    cambios a

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -24,25 +24,25 @@
             <div class="col-lg-3 wow fadeIn" data-wow-delay=".1s">
                 <div class="d-flex counter">
                     <h1 class="me-3 text-primary counter-value">99</h1>
-                    <h5 class="text-white mt-1">Success in getting happy customer</h5>
+                    <h5 class="text-white mt-1">Éxito en conseguir un cliente satisfecho</h5>
                 </div>
             </div>
             <div class="col-lg-3 wow fadeIn" data-wow-delay=".3s">
                 <div class="d-flex counter">
                     <h1 class="me-3 text-primary counter-value">25</h1>
-                    <h5 class="text-white mt-1">Thousands of successful business</h5>
+                    <h5 class="text-white mt-1">Miles de negocios exitosos</h5>
                 </div>
             </div>
             <div class="col-lg-3 wow fadeIn" data-wow-delay=".5s">
                 <div class="d-flex counter">
                     <h1 class="me-3 text-primary counter-value">120</h1>
-                    <h5 class="text-white mt-1">Total clients who love HighTech</h5>
+                    <h5 class="text-white mt-1">Clientes totales que aman la alta tecnología</h5>
                 </div>
             </div>
             <div class="col-lg-3 wow fadeIn" data-wow-delay=".7s">
                 <div class="d-flex counter">
                     <h1 class="me-3 text-primary counter-value">5</h1>
-                    <h5 class="text-white mt-1">Stars reviews given by satisfied clients</h5>
+                    <h5 class="text-white mt-1">Reseñas de estrellas dadas por clientes satisfechos.</h5>
                 </div>
             </div>
         </div>
