c3120d24af7f150c59e4dae15c2111031634ea9f
juan torres <juanonfleekmedia@gmail.com>
2023-10-26 14:40:26 -0500

    cambios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -435,32 +435,11 @@
       <div class="modal-body" style="padding: 0;">
         <img src="{{asset('img/octubre.png')}}" width="100%" alt="">
         <a href="https://wa.me/51978209130" class="me-2"><button type="button" class="flotante btn btn-secondary rounded-pill carousel-content-btn1 animated fadeInLeft">Click aquí</button></a>
-        <!-- <a href="https://wa.me/51978209130" target="_blank" class="btn bg-secondary text-white flotante">Click aquí</a> -->
+        
       </div>
-      <!-- <div class="modal-footer">
-        <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" data-bs-dismiss="modal">Open second modal</button>
-      </div> -->
     </div>
   </div>
 </div>
-<!-- <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalToggleLabel2">Modal 2</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Hide this modal and show the first with the button below.
-      </div>
-      <div class="modal-footer">
-        <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal" data-bs-dismiss="modal">Back to first</button>
-      </div>
-    </div>
-  </div>
-</div> -->
-<!-- <a class="btn btn-primary" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Open first modal</a> -->
-<!-- fin modal -->
 
 @endsection
 
